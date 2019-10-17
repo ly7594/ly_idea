07dd6792f8f9aec376c033c62645dc1de9d85190
ly7594 <LeaY11@163.com>
2019-10-17 11:31:54 +0800

    git send local repository

---
+++ ssm/src/main/java/Demo.java
@@ -0,0 +1,11 @@
+/**
+ * @author 毅哥哥
+ * @site
+ * @company
+ * @create 2019-10-17 11:29
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("git idea test");
+    }
+}
